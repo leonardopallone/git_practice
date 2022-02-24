6d99897600bcdd292863e48746e8b969549e0983
Leonardo Pallone <leonardopallone@gmaill.com>
2022-02-24 12:34:26 -0700

    added Gems

--- main.rb
+++ main.rb
@@ -1,21 +1,46 @@
+# def puts_git(cmd)
+#   puts `git #{cmd} -h`
+#   menu
+# end
+
+# def menu
+#   puts '1: Enter git command'
+#   puts '2: Exit'
+#   choice = gets.to_i
+#   case choice
+#   when 1
+#     puts 'Enter git command'
+#     puts_git(gets.strip)
+#     menu
+#   when 2
+#     exit
+#   else
+#     puts 'Invalid choice'
+#     menu
+#   end
+# end
+
+# # menu
+require 'colorize'
+
 def puts_git(cmd)
   puts `git #{cmd} -h`
   menu
 end
 
 def menu
-  puts '1: Enter git command'
-  puts '2: Exit'
+  puts '1: Enter git command'.colorize(:cyan)
+  puts '2: Exit'.colorize(:red)
   choice = gets.to_i
   case choice
   when 1
-    puts 'Enter git command'
+    puts 'Enter git command'.colorize(:green)
     puts_git(gets.strip)
     menu
   when 2
     exit
   else
-    puts 'Invalid choice'
+    puts 'Invalid choice'.colorize(:red)
     menu
   end
 end
